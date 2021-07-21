5c9f443176267e787ce6151a8cabd113156900ae
ArnoldoRicardo <anyback@gmail.com>
2021-07-21 16:59:08 -0500

    update dabase url

--- api/database.py
+++ api/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.environ['SQLALCHEMY_DATABASE_URI']
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    'DATABASE_URL?sslmode=require').replace('postgres://', 'postgresql://')
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
