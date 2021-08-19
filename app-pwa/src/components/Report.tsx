a40808e174bdcfd0a8687392a530a379de310b68
arnoldoRicardo <anyback@gmail.com>
2021-08-18 19:46:26 -0500

    update reporte

--- app-pwa/src/components/Report.tsx
+++ app-pwa/src/components/Report.tsx
@@ -6,10 +6,30 @@ type ReportParams = {
     fecha: string;
 };
 
-export const Report = () => {
+type Venta = {
+    nombre: string;
+    cantidad: number;
+    total: number;
+    id: number;
+    note_id: number;
+};
+
+type Note = {
+    pk: number;
+    cliente: string;
+    total: number;
+    anticipo: number;
+    id: number;
+    date: string;
+    ventas: Venta[];
+};
+
+export const Report: React.FC = () => {
     const { fecha } = useParams<ReportParams>();
 
-    const [reports, setReports] = useState([]);
+    const [total, setTotal] = useState<number>(0);
+
+    const [reports, setReports] = useState<Note[]>([]);
     console.log(reports);
     useEffect(() => {
         const getNotes = async () => {
@@ -18,6 +38,8 @@ export const Report = () => {
                     `https://fathomless-atoll-57807.herokuapp.com/note?fecha=${fecha}`
                 );
                 setReports(response.data);
+                const newTotal: number = response.data.reduce((t: number, nota: Note) => t+nota.total, 0)
+                setTotal(newTotal)
             } catch (e) {
                 console.log(e);
             }
@@ -27,11 +49,22 @@ export const Report = () => {
 
     return (
         <>
-            {reports && reports.map((report) => <>#hola</>)}
-            #1 juan $140
-            <ul>
-                <li>2mt lona $70</li>
-            </ul>
+            ventas del dia ${total}<br/>
+            {reports &&
+                reports.map((report) => (
+                    <>
+                        #{report.pk} {report.cliente} ${report.total}
+                        {report.ventas &&
+                            report.ventas.map((venta) => (
+                                <ul>
+                                    <li>
+                                        {venta.cantidad} - {venta.nombre} $
+                                        {venta.total}
+                                    </li>
+                                </ul>
+                            ))}
+                    </>
+                ))}
         </>
     );
 };
